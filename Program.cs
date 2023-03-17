293b4b0f84406a151afc55894a1190dccd8c3252
Martin <Martin@liquidicestudios.com>
2023-03-17 14:14:03 +0100

    Created a function that takes an int parameter which then calculates the fibonnaci number of that parameter using recursive

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    //Function that take in a int parameter 
+    public int Fibonnaci(int number)
+    {
+        //if statement that prevents a negative number input
+        if (number < 1) return 0;
+        // if statements that begins the fibonnaci
+        if (number == 1) return 0;
+        if (number == 2) return 1;
+        // The function returns a recursive to create the fibonnaci
+        return Fibonnaci(number -1) + Fibonnaci(number -2);
+    }
+
+    public static void Main(string[] args)
+    {
+        Program p = new Program();
+        Console.WriteLine(p.Fibonnaci(30));
+    }
+}
